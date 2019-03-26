f8958294e28932587239f8f95f4640232a038a29
Jay Upadhyay <jay@getventive.com>
2019-03-26 16:40:11 -0600

    Broker Sanity fixed issue with archived deals page link leftnavbar

---
+++ src/main/java/com/qa/pages/Broker_BuyerUC_Submission_DealsPage.java
@@ -0,0 +1,5 @@
+package com.qa.pages;
+
+public class Broker_BuyerUC_Submission_DealsPage {
+
+}
---
+++ src/test/java/com/qa/testcases/Broker_BuyerUC_Submission_DealsPageTest.java
@@ -0,0 +1,5 @@
+package com.qa.testcases;
+
+public class Broker_BuyerUC_Submission_DealsPageTest {
+
+}
